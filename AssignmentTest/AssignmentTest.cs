bf91701fb50ac70cbe96b4baf537e677799cd4bb
parikhsakshi <131210174+parikhsakshi@users.noreply.github.com>
2023-06-11 16:13:27 +0000

    Updated working

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -29,5 +29,98 @@ namespace AssignmentTest
             robot1.Y = -5;
             Assert.AreEqual(robot1.Y, -5);
         }
+
+        [TestMethod]
+        public void OnCommandTest()
+        {
+            Robot testRobot = new Robot();
+            // How do I load a command into this robot?
+
+            Assert.AreEqual(testRobot.IsPowered, false);
+            testRobot.LoadCommand(new OnCommand());
+            testRobot.Run();
+            Assert.AreEqual(testRobot.IsPowered, true);
+            //Robot executes the OnCommand again (memory isn't clear between runs)
+            testRobot.Run();
+            //Verifies if a turned on Robot is turned on again, it stays powered on
+            Assert.AreEqual(testRobot.IsPowered, true);
+        }
+
+        [TestMethod]
+        public void OffCommandTest()
+        {
+            Robot testRobot = new Robot();
+            Assert.AreEqual(testRobot.IsPowered, false);
+            testRobot.IsPowered = true;
+            testRobot.LoadCommand(new OffCommand());
+            testRobot.Run();
+            Assert.AreEqual(testRobot.IsPowered, false);
+            testRobot.Run();
+            Assert.AreEqual(testRobot.IsPowered, false);
+        }
+
+        [TestMethod]
+        public void MoveWestCommandTest()
+        {
+            Robot testRobot = new Robot();
+            Assert.AreEqual(testRobot.X, 0);
+            testRobot.LoadCommand(new WestCommand());
+            testRobot.Run();
+            //Robot ispowered off by default
+            Assert.AreEqual(testRobot.X, 0); //Robot should not move if it isn't turned on
+            testRobot.IsPowered = true;
+            testRobot.Run();
+            Assert.AreEqual(testRobot.X, -1);
+            testRobot.Run();
+            Assert.AreEqual(testRobot.X, -2);
+        }
+
+        [TestMethod]
+        public void MoveNorthCommandTest()
+        {
+            Robot testRobot = new Robot();
+            Assert.AreEqual(testRobot.Y, 0);
+            testRobot.LoadCommand(new NorthCommand());
+            testRobot.Run();
+            //Robot ispowered off by default
+            Assert.AreEqual(testRobot.Y, 0); //Robot should not move if it isn't turned on
+            testRobot.IsPowered = true;
+            testRobot.Run();
+            Assert.AreEqual(testRobot.Y, 1);
+            testRobot.Run();
+            Assert.AreEqual(testRobot.Y, 2);
+        }
+
+        [TestMethod]
+        public void MoveEastCommandTest()
+        {
+            Robot testRobot = new Robot();
+            Assert.AreEqual(testRobot.X++, 0);
+            testRobot.LoadCommand(new EastCommand());
+            testRobot.Run();
+            //Robot is powered off by default
+            Assert.AreEqual(testRobot.X++, 0); //Robot should not move if it isn't turned on
+            testRobot.IsPowered = true;
+            testRobot.Run();
+            Assert.AreEqual(testRobot.X++, 1);
+            testRobot.Run();
+            Assert.AreEqual(testRobot.X++, 2);
+        }
+
+        [TestMethod]
+        public void MoveSouthCommandTest()
+        {
+            Robot testRobot = new Robot();
+            Assert.AreEqual(testRobot.Y--, 0);
+            testRobot.LoadCommand(new SouthCommand());
+            testRobot.Run();
+            //Robot is powered off by default
+            Assert.AreEqual(testRobot.Y--, 0); //Robot should not move if it isn't turned on
+            testRobot.IsPowered = true;
+            testRobot.Run();
+            Assert.AreEqual(testRobot.Y--, -1);
+            testRobot.Run();
+            Assert.AreEqual(testRobot.Y--, -2);
+        }
     }
 }
